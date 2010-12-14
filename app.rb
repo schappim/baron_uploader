dd74381aec2b7f4ee4f6c12570d679eb85c75a58
schappim <schappi@gmail.com>
2010-12-14 17:42:54 +1100

    first commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sinatra'
+
+  get '/' do
+    erb :index
+  end
+
+  put '/' do
+    
+    theFile = request.body.read
+    puts "uploaded #{env['HTTP_X_FILENAME']} - #{request.body.read.size} bytes"    
+    aFile = File.new(env['HTTP_X_FILENAME'], "wb")
+    aFile.write(theFile)
+    aFile.close
+
+  end
